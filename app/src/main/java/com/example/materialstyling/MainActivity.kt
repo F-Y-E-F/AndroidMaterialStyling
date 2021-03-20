5607706b9b23431f0bfac71de9548d6ac26a3380
andryue <kacperwojak17@gmail.com>
2021-03-20 10:26:25 +0100

    add nice text input layout like spinner - betteer than spinner i meant

--- app/src/main/java/com/example/materialstyling/MainActivity.kt
+++ app/src/main/java/com/example/materialstyling/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.materialstyling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.materialstyling.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        val languages = resources.getStringArray(R.array.languages)
+        val arrayAdapter = ArrayAdapter(applicationContext,R.layout.dropdown_item,languages)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
+    }
+
+
 }
